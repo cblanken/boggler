fe1f1c2b9725415f8cd1bf4f72889a83ad4d238c
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2022-09-21 10:04:02 -0400

    Fix MAX_WORD_LENGTH parsing

--- src/boggler/boggler.py
+++ src/boggler/boggler.py
@@ -11,22 +11,24 @@ if __name__ == '__main__':
     board = read_boggle_file(Path(sys.argv[1]))
     if len(sys.argv) == 3:
         boggle_board = BoggleBoard(board)
-        print(boggle_board)
-        boggle_tree = build_full_boggle_tree(boggle_board, Path(sys.argv[2]))
     elif len(sys.argv) == 4:
-        try :
+        try:
             boggle_board = BoggleBoard(board, int(sys.argv[3]))
-            boggle_tree = build_full_boggle_tree(boggle_board, Path(sys.argv[2]))
-
-            print("\nBOARD")
-            print(boggle_board)
-
-            for start_pos, tree in boggle_tree.items():
-                print(f"\nStarting @ {start_pos}...")
-                for word in tree.word_paths:
-                    print(f"{word[0]: <{boggle_board.max_word_len}}: {word[1]}")
-
-        except ValueError as e:
-            print("The [MAX_WORD_LENGTH] argument must be an integer.")
-            print("Please try again.")
+        except ValueError:
+            print("Invalid MAX_WORD_LENGTH. Please try again with a valid integer.")
             sys.exit(1)
+    try:
+        boggle_tree = build_full_boggle_tree(boggle_board, Path(sys.argv[2]))
+
+        print("\nBOARD")
+        print(boggle_board)
+
+        for start_pos, tree in boggle_tree.items():
+            print(f"\nStarting @ {start_pos}...")
+            for word in tree.word_paths:
+                print(f"{word[0]: <{boggle_board.max_word_len}}: {word[1]}")
+
+    except ValueError as e:
+        print("The [MAX_WORD_LENGTH] argument must be an integer.")
+        print("Please try again.")
+        sys.exit(1)
