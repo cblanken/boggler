6f4d4a69a05db6088630f8735e74525b4f77208b
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2023-03-28 19:05:08 -0400

    Add WordTree data structure and helper functions

--- src/boggler/c/boggler.c
+++ src/boggler/c/boggler.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 #include "board.h"
+#include "tree.h"
 
 int main(int argc, char **argv) {
     const uint8_t BOARD_SIZE = 4;
@@ -17,9 +18,20 @@ int main(int argc, char **argv) {
     struct BoardCell **cells = malloc(sizeof(struct BoardCell*) * BOARD_SIZE * BOARD_SIZE);
     for (uint8_t i = 0; i < BOARD_SIZE * BOARD_SIZE; i++) {
         cells[i] = create_board_cell(letters[i], (int)(i / BOARD_SIZE), i % BOARD_SIZE);
+        print_boardcell(cells[i]);
     }
 
     Board board = { .height = 4, .width = 4, .cells = cells };
     print_board(&board);
     free(cell_letters);
+
+    struct WordNode *root = malloc(sizeof(struct WordNode));
+    init_wordnode(root, false, cells[0], NULL, NULL, 0);
+    print_wordnode(root);
+    add_child_node(root, cells[5], false);
+    //add_child_node(root->children[0], cells[6], false);
+    print_wordnode(root);
+    print_wordnode_path(root->children[0]);
+
+    free(root);
 }
